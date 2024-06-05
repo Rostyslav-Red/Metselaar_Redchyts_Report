80c6ab93021ac867625158d31a7110fe3b59daad
Rostyslav <r.redchyts@gmail.com>
2024-06-06 01:32:28 +0200

    Commented the code + added a new function
    The new function takes some binary arguments that allow the user to choose between the Forward selection and Backward elimination vs using p-values and using adjusted r^2 values.
    Based on those values, the function performs a stepwise regression

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from scipy.stats import probplot
 
 sns.set_style("whitegrid")
 
-houseData = pd.read_csv('Rostyslav_Redchyts-Niels_Metselaar-data.csv')
+# Retrieving the data from the database
+raw_houseData = pd.read_csv('Rostyslav_Redchyts-Niels_Metselaar-data.csv')
+houseData = raw_houseData.drop(columns=['id', 'date', 'waterfront', 'zipcode', 'lat', 'long'])
 print(houseData.tail())
 
 """
@@ -28,89 +30,148 @@ Explanatory Variables:
 12.	Grade of building’s construction and design (scale of 1-13) (grade)
 """
 
-column_names = ['bathrooms', 'bedrooms', 'I(bedrooms**2)', 'sqft_living', 'sqft_basement_dummy',
-           'sqft_above', 'floors', 'I(floors**2)', 'view', 'condition', 'yr_built', 'yr_renovated_dummy', 'grade']
+# The list of the variables that are considered for the model
 
-print(len(column_names))
+column_names = ['bathrooms', 'bedrooms', 'I(bedrooms ** 2)', 'sqft_living', 'sqft_basement_dummy', 'sqft_above', 'floors',
+                'I(floors ** 2)', 'view', 'condition', 'yr_built', 'yr_renovated_dummy', 'grade', 'sqft_living15',
+                'sqft_lot15']
 
-formula = ('price ~ bathrooms + bedrooms + I(bedrooms**2) + sqft_living + sqft_basement_dummy + '
-           'sqft_above + floors + I(floors**2) + view + condition + yr_built + '
-           'yr_renovated_dummy + grade')
+formula = ('price ~ bathrooms + bedrooms + I(bedrooms ** 2) + sqft_living + sqft_basement_dummy + '
+           'sqft_above + floors + I(floors ** 2) + view + condition + yr_built + '
+           'yr_renovated_dummy + grade + sqft_living15 + sqft_lot15')
 
+# Adding the column, named 'sqft_basement_dummy' to the database. This variable equates to 1 if the property has a
+# basement and 0 when it doesn't
 houseData['sqft_basement_dummy'] = (houseData['sqft_basement'] > 0).astype(int)
+
+# Adding the column, named 'yr_renovated_dummy', which equates to the number of years that have passed since the
+# last renovation of the property. If the renovation never happened, the output is just the age of the property.
 current_year = 2024
 houseData['yr_renovated_dummy'] = np.where(houseData['yr_renovated'] == 0,
                                            current_year - houseData['yr_built'],
                                            current_year - houseData['yr_renovated'])
-# for i in column_names:
-#     sns.scatterplot(data=houseData, x = i, y = 'price')
-#     plt.show()
 
 
-#checking normality of response variable
-sm.qqplot(houseData['price'], line='s')
-plt.title('QQ plot price distribution')
-plt.show()
-
-sns.displot(houseData['price'], bins=50)
-plt.title('The distribution of housing prices')
-plt.show()
+def basic_plot():  # Plotting every variable to 'price'
+    for column in column_names:
+        sns.scatterplot(data=houseData, x=column, y='price')
+        plt.show()
 
 
-model = sm.formula.ols(formula = formula, data = houseData)
-model_fit = model.fit()
+# basic_plot()
 
-# optimal_flag = False
-#
-# useful_columns = column_names.copy()
-# prev_adj_r2 = model_fit.rsquared_adj
-#
-# while not optimal_flag:
-#     results = dict()
-#
-#     for i in useful_columns:
-#         shortened_columns = useful_columns.copy()
-#         shortened_columns.remove(i)
-#         new_formula = "price ~ " + " + ".join(shortened_columns)
-#         new_adj_r2 = sm.formula.ols(formula=new_formula, data=houseData).fit().rsquared_adj
-#         results[i] = new_adj_r2
-#
-#     best_fit = (max(results, key=results.get), results[max(results, key=results.get)])
-#     print(results)
-#     print(best_fit)
-#     if best_fit[1] >= prev_adj_r2:
-#         useful_columns.remove(best_fit[0])
-#         prev_adj_r2 = best_fit[1]
-#     else:
-#         optimal_flag = True
-#
-# print(useful_columns, len(useful_columns))
 
-optimal_flag = False
+def response_normality_check():  # Checking the normality of the response variable
+    sm.qqplot(houseData['price'], line='s')
+    plt.title('QQ plot price distribution')
+    plt.show()
 
-useful_columns = []
-remaining_columns = column_names.copy()
-prev_adj_r2 = 0
+    sns.displot(houseData['price'], bins=50)
+    plt.title('The distribution of housing prices')
+    plt.show()
 
-while not optimal_flag and remaining_columns:
-    results = dict()
 
-    for i in remaining_columns:
-        new_formula = "price ~ " + " + ".join(useful_columns + [i])
-        new_adj_r2 = sm.formula.ols(formula=new_formula, data=houseData).fit().rsquared_adj
-        results[i] = new_adj_r2
+# response_normality_check()
 
-    best_fit = (max(results, key=results.get), results[max(results, key=results.get)])
-    print(results)
-    print(best_fit)
-    if best_fit[1] >= prev_adj_r2:
-        remaining_columns.remove(best_fit[0])
-        useful_columns.append(best_fit[0])
-        prev_adj_r2 = best_fit[1]
+
+def stepwise_regression(data, columns, response, forward=True, use_p=True):
+    def construct_formula(rem_c, used_c, current_c=''):
+        if forward:
+            if used_c:
+                return response + " ~ " + " + ".join(used_c) + " + " + current_c
+            else:
+                return response + " ~ " + current_c
+        else:
+            if use_p:
+                return response + " ~ " + " + ".join(rem_c)
+            else:
+                rem_c.remove(current_c)
+                return response + " ~ " + " + ".join(rem_c)
+
+    def evaluate_change(current_formula, c):
+        fitted_model = sm.formula.ols(formula=current_formula, data=data).fit()
+        if use_p:
+            # print(fitted_model.pvalues)
+            return fitted_model.pvalues[c]
+        else:
+            return fitted_model.rsquared_adj
+
+    print("MODE: ", ("Backward", "Forward")[int(forward)], ("Adj. R^2", "p-value")[use_p])
+
+    optimal_flag = False
+
+    remaining_columns = columns.copy()
+    used_columns = list()
+
+    if not forward:
+        prev_best_assessment = sm.formula.ols(formula=response + " ~ " + " + ".join(remaining_columns),
+                                              data=data).fit().rsquared_adj
     else:
-        optimal_flag = True
+        prev_best_assessment = 0
 
-print(useful_columns, len(useful_columns))
+    # proper_length_flag = (use_p and remaining_columns) or (not use_p and not forward and len(remaining_columns)>1)
 
-print(model_fit.summary())
-print(model_fit.rsquared_adj, model_fit.rsquared)
+    while not optimal_flag and remaining_columns:
+        print("Remaining_columns", remaining_columns)
+        print("Used_columns", used_columns, end="\n\n")
+        results = dict()
+
+        for column in remaining_columns:
+            print(f"\tColumn: {column}")
+            temp_remaining_columns = remaining_columns.copy()
+            temp_remaining_columns.remove(column)
+
+            temp_formula = construct_formula(remaining_columns.copy(), used_columns, column)
+            print(f"\tTemp_formula: {temp_formula}")
+            temp_assessment = evaluate_change(temp_formula, column)
+            print(f"\tTemp_assessment: {temp_assessment}")
+            results[column] = temp_assessment
+
+        print(f"Results: {results}")
+        min_fit = (min(results, key=results.get), results[min(results, key=results.get)])
+
+        max_fit = (max(results, key=results.get), results[max(results, key=results.get)])
+
+        print(f"Min: {min_fit}, Max: {max_fit}")
+
+        if forward and use_p:
+            if min_fit[1] < 0.05:
+                used_columns.append(min_fit[0])
+                remaining_columns.remove(min_fit[0])
+            else:
+                optimal_flag = True
+        elif forward and (not use_p):
+            if max_fit[1] >= prev_best_assessment:
+                used_columns.append(max_fit[0])
+                remaining_columns.remove(max_fit[0])
+                prev_best_assessment = max_fit[1]
+            else:
+                optimal_flag = True
+        elif (not forward) and use_p:
+            if min_fit[1] > 0.05:
+                remaining_columns.remove(min_fit[0])
+            else:
+                optimal_flag = True
+        elif (not forward) and (not use_p):
+            if max_fit[1] > prev_best_assessment:
+                print(f"\tPrev_max: {prev_best_assessment}")
+                remaining_columns.remove(max_fit[0])
+                prev_best_assessment = max_fit[1]
+            else:
+                optimal_flag = True
+
+    if forward:
+        return used_columns
+    else:
+        return remaining_columns
+
+
+print(
+    "Forward p-values: \n\t" + str(stepwise_regression(houseData, column_names, "price", True, True)) + "\n" +
+    "Forward Adjusted R^2: \n\t" + str(stepwise_regression(houseData, column_names, "price", True, False)) + "\n" +
+    "Backward p-values: \n\t" + str(stepwise_regression(houseData, column_names, "price", False, True)) + "\n" +
+    "Backward Adjusted R^2: \n\t" + str(stepwise_regression(houseData, column_names, "price", False, False))
+)
+
+# print(model_fit.summary())
+# print(model_fit.rsquared_adj, model_fit.rsquared)
