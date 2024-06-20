35462c219e5edfc415702de1a3113b430f43b515
Rostyslav <r.redchyts@gmail.com>
2024-06-20 21:23:32 +0200

    Added the assumption checks

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ sns.set_style("whitegrid")
 raw_houseData = pd.read_csv('Rostyslav_Redchyts-Niels_Metselaar-data.csv')
 houseData = raw_houseData.drop(columns=['id', 'date', 'zipcode', 'lat', 'long'])
 
-#####
-
-sns.scatterplot(data=houseData, x='sqft_basement', y='price', alpha=0.5, edgecolor=None,
-                hue='price', palette='coolwarm', legend=None).set_title('Price vs. sqft_basement')
-plt.xlabel('sqft_basement')
-plt.ylabel('Price')
-sns.regplot(data=houseData, x='sqft_basement', y='price', scatter=False, color='black')
-plt.show()
-
-#####
-
 houseData.drop(index=houseData[raw_houseData['bedrooms'] == 33].index, inplace=True)
 houseData.reset_index(drop=True, inplace=True)
 
@@ -69,10 +58,10 @@ houseData['yr_renovated_dummy'] = np.where(houseData['yr_renovated'] == 0,
 
 houseData['log_price'] = np.log(houseData['price'])
 
-model = sm.formula.ols(formula=formula, data=houseData).fit()
-print(f"Regular R^2: {model.rsquared}")
-print(f"Adjusted R^2: {model.rsquared_adj}")
-print(f"Calculated Adj. R^2: {1 - (1 - model.rsquared) * (21613 - 1) / (21613 - 15 - 1)}")
+# model = sm.formula.ols(formula=formula, data=houseData).fit()
+# print(f"Regular R^2: {model.rsquared}")
+# print(f"Adjusted R^2: {model.rsquared_adj}")
+# print(f"Calculated Adj. R^2: {1 - (1 - model.rsquared) * (21613 - 1) / (21613 - 15 - 1)}")
 
 
 def basic_plot():
@@ -125,7 +114,7 @@ def stepwise_regression(data, columns, response, forward=True, use_p=True):
         else:
             return fitted_model.rsquared_adj
 
-    print("MODE: ", ("Backward", "Forward")[int(forward)], ("Adj. R^2", "p-value")[use_p])
+    # print("MODE: ", ("Backward", "Forward")[int(forward)], ("Adj. R^2", "p-value")[use_p])
 
     optimal_flag = False
 
@@ -141,34 +130,34 @@ def stepwise_regression(data, columns, response, forward=True, use_p=True):
     # proper_length_flag = (use_p and remaining_columns) or (not use_p and not forward and len(remaining_columns)>1)
 
     while not optimal_flag and remaining_columns:
-        print("Remaining_columns", remaining_columns)
-        print("Used_columns", used_columns, end="\n\n")
+        # print("Remaining_columns", remaining_columns)
+        # print("Used_columns", used_columns, end="\n\n")
         results = dict()
 
         for column in remaining_columns:
-            print(f"\tColumn: {column}")
+            # print(f"\tColumn: {column}")
             temp_remaining_columns = remaining_columns.copy()
             temp_remaining_columns.remove(column)
 
             temp_formula = construct_formula(remaining_columns.copy(), used_columns, column)
-            print(f"\tTemp_formula: {temp_formula}")
+            # print(f"\tTemp_formula: {temp_formula}")
             temp_assessment = evaluate_change(temp_formula, column)
-            print(f"\tTemp_assessment: {temp_assessment}")
+            # print(f"\tTemp_assessment: {temp_assessment}")
             results[column] = temp_assessment
 
-        print(f"Results: {results}")
+        # print(f"Results: {results}")
         min_fit = (min(results, key=results.get), results[min(results, key=results.get)])
 
         max_fit = (max(results, key=results.get), results[max(results, key=results.get)])
 
-        print(f"Min: {min_fit}, Max: {max_fit}")
+        # print(f"Min: {min_fit}, Max: {max_fit}")
 
         if forward and use_p:
             if min_fit[1] < 0.05:
                 used_columns.append(min_fit[0])
                 remaining_columns.remove(min_fit[0])
             else:
-                print("optimal")
+                # print("optimal")
                 optimal_flag = True
         elif forward and (not use_p):
             if max_fit[1] >= prev_best_assessment:
@@ -184,7 +173,7 @@ def stepwise_regression(data, columns, response, forward=True, use_p=True):
                 optimal_flag = True
         elif (not forward) and (not use_p):
             if max_fit[1] > prev_best_assessment:
-                print(f"\tPrev_max: {prev_best_assessment}")
+                # print(f"\tPrev_max: {prev_best_assessment}")
                 remaining_columns.remove(max_fit[0])
                 prev_best_assessment = max_fit[1]
             else:
@@ -203,14 +192,106 @@ def stepwise_regression(data, columns, response, forward=True, use_p=True):
 #     "Backward Adjusted R^2: \n\t" + str(stepwise_regression(houseData, column_names, "log_price", False, False))
 # )
 
+columns = ['bathrooms', 'bedrooms', 'sqft_living', 'waterfront', 'sqft_basement',
+                'sqft_above', 'floors', 'view', 'condition', 'yr_built', 'yr_renovated', 'grade', 'sqft_living15']
 
-# print(model_fit.summary())
-# print(model_fit.rsquared_adj, model_fit.rsquared)
+used_columns = stepwise_regression(houseData, columns, 'price', True, True)
 
-# test_f = 'log_price ~ bathrooms + sqft_living + yr_built + grade + view + I(floors ** 2) + sqft_living15 + sqft_basement_dummy + waterfront + condition + bedrooms + sqft_above + I(bedrooms ** 2) + floors'
-# model_test = sm.formula.ols(formula=test_f, data=houseData).fit()
-# print(f"Adjusted R^2: {model.rsquared_adj}")
-#
-# test_f += ' + yr_renovated_dummy'
-# model_test_2 = sm.formula.ols(formula=test_f, data=houseData).fit()
-# print(model_test_2.pvalues['yr_renovated_dummy'])
+formula = 'price ~ ' + ' + '.join(used_columns)
+
+model = sm.formula.ols(formula=formula, data=houseData).fit()
+
+print(formula)
+"""
+price ~ bathrooms + sqft_living + waterfront + yr_built + grade + view + bedrooms + floors + condition + sqft_living15 + sqft_above + sqft_basement + yr_renovated
+"""
+# temp = (model.params[i] * houseData[val] for i, val in enumerate(used_columns))
+# predicted_score_values = model.params[0] + temp
+
+
+predicted_score_values = model.params.iloc[0]
+for i, val in enumerate(used_columns):
+    predicted_score_values += model.params.iloc[i + 1] * houseData[val]
+print(predicted_score_values)
+regular_residuals = houseData['price']-predicted_score_values
+absolute_residuals = np.abs(regular_residuals)
+
+# plt.subplot
+
+
+def check_assumptions(rows, cols, variables):
+    used_columns = stepwise_regression(houseData, variables, 'price', True, True)
+    formula = 'price ~ ' + ' + '.join(used_columns)
+
+    model = sm.formula.ols(formula=formula, data=houseData).fit()
+
+    print(formula)
+    """
+    price ~ bathrooms + sqft_living + waterfront + yr_built + grade + view + bedrooms + floors + condition + sqft_living15 + sqft_above + sqft_basement + yr_renovated
+    """
+    # temp = (model.params[i] * houseData[val] for i, val in enumerate(used_columns))
+    # predicted_score_values = model.params[0] + temp
+
+    predicted_score_values = model.params.iloc[0]
+    for i, val in enumerate(used_columns):
+        predicted_score_values += model.params.iloc[i + 1] * houseData[val]
+    print(predicted_score_values)
+    regular_residuals = houseData['price'] - predicted_score_values
+    absolute_residuals = np.abs(regular_residuals)
+
+    fig, axes = plt.subplots(rows, cols, figsize = (18, 20))
+
+    # Checking if variability of the residuals is nearly constant
+    #(scatterplot of the (absolute) residuals (y-axis) against the predicted values (x-axis))
+
+    # plt.figure(figsize=(8, 6))
+    axes[0, 0].scatter(predicted_score_values, absolute_residuals)
+    axes[0, 0].set_title('Absolute residuals vs. Predicted values')
+    axes[0, 0].set_xlabel('Predicted values')
+
+    axes[0, 0].set_ylabel('Absolute Residuals')
+    # plt.show()
+
+    # normality of the residuals (a histogram and QQ-plot of the residuals)
+
+    axes[0, 1].hist(regular_residuals, bins=100, edgecolor='k')
+    axes[0, 1].set_title("Histogram of Residuals")
+    axes[0, 1].set_xlabel('Residuals')
+    axes[0, 1].set_ylabel('Frequency')
+
+    sm.qqplot(regular_residuals, line='s', ax=axes[1, 0])
+    axes[1, 0].set_title('QQ plot Residuals')
+    # print(max(regular_residuals), min(regular_residuals))
+
+    # Residuals are independent (scatterplot of the residuals (y-axis) against the order of collection (x-axis))
+
+    axes[1, 1].scatter(np.arange(len(regular_residuals)), regular_residuals)
+    axes[1, 1].set_title('Residuals at the order of collection')
+    axes[1, 1].set_xlabel('Order of collection')
+    axes[1, 1].set_ylabel('Residuals')
+
+    row, col = 2, 0
+    temp = used_columns.copy()
+    temp.remove('waterfront')
+    for var in temp:
+        if col == 2:
+            row += 1
+            col = 0
+        axes[row, col].scatter(houseData[var], regular_residuals)
+        axes[row, col].set_title(f'Residuals vs {var}')
+        axes[row, col].set_xlabel(var)
+        axes[row, col].set_ylabel('Residuals')
+        col += 1
+
+    sns.boxplot(x='waterfront', y=regular_residuals, data=houseData, ax=axes[8, 0])
+    axes[8, 0].set_title('Boxplot of Residuals by Waterfront')
+    axes[8, 0].set_xlabel('Waterfront')
+    axes[8, 0].set_ylabel('Residuals')
+    """
+    [2, 0], [2, 1], [3, 0], ... , [7, 1]"""
+
+    plt.tight_layout()
+    plt.show()
+
+
+check_assumptions(9, 2, columns)
